a99826c2587ece26418fc4698759270a30e3a589
Thomas <Thomas@Rising.de>
2016-09-08 20:56:15 +0200

    Controls

---
+++ RisingTerra/Assets/MyAssets/TileRenderer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(RectTransform))]
+public class TileRenderer : MonoBehaviour
+{
+
+    public Sprite sprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        try
+        {
+            var rect = this.GetComponent<RectTransform>();
+            var parent = new GameObject("parent");
+
+           // for (int i = 0; i < 10; i++)
+           // {
+                var go = new GameObject("Test_" );
+                var sp = go.AddComponent<SpriteRenderer>();
+                sp.sprite = sprite;
+
+                go.transform.parent = parent.transform;
+            //}
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ RisingTerra/Assets/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Transform _groundCheck;
+
+    private RaycastHit2D _grounded;
+
+    private Rigidbody2D _rigiBody;
+
+    private Transform _hero;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._groundCheck = transform.FindChild("groundCheck");
+        this._rigiBody = this.GetComponent<Rigidbody2D>();
+        this._hero = transform.FindChild("Hero");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._grounded = Physics2D.Linecast(transform.position, this._groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButton("Horizontal") && _grounded)
+        {
+            var axis = Input.GetAxis("Horizontal");
+
+            if (axis > 0)
+            {
+                this._rigiBody.velocity = Vector2.right * 2;
+
+                var stateInfo = this._hero.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+                this._hero.GetComponent<Animator>().enabled = true;
+                this._hero.GetComponent<SpriteRenderer>().flipX = true;
+                this._hero.GetComponent<Animator>().Play("Hero_Walk");
+            }
+            else
+            {
+                this._rigiBody.velocity = Vector2.left * 2;
+                this._hero.GetComponent<Animator>().enabled = true;
+                this._hero.GetComponent<SpriteRenderer>().flipX = false;
+                this._hero.GetComponent<Animator>().Play("Hero_Walk");
+            }
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            this._hero.GetComponent<Animator>().Play("Hero_Idle");
+            //this._hero.GetComponent<Animator>().enabled = false;
+        }
+    }
+}
